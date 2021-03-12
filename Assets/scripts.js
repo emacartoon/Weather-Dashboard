2879678663130ca90c4718611c400e1093e5c471
EmaCartoon <emacartoon@gmail.com>
2021-03-11 22:48:34 -0700

    Worked with a homework group to figure out functionality of the script. Saving this before anything happens.

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,26 +1,161 @@
 var api_key = "f45ebe8eb4e6aec0968bafa6e60740c7";
+var forecastContainer = document.querySelector("#forecast-container");
 var city = "Mesa";
+var $masthead = document.querySelector("#masthead");
+var $cityText = document.querySelector("city-text");
+var historyContainer = document.querySelector("#cityhistory")
+var $mastHead = document.querySelector("#masthead")
+var dhistory = JSON.parse(localStorage.getItem("history")) || [];
 
-var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+// Get today's weather function
+function getWeather(city) {
+  var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  fetch(currentWeatherUrl)
+    .then((data) => data.json())
+    .then(function (weather) {
+      console.log(weather);
 
-function getWeather(city){
-    fetch(currentWeatherUrl)
-      .then((data) => data.json())
-      .then(function (weather) {
-        console.log(weather);
-        var lat = weather.coord.lat;
-        var lon = weather.coord.lon;
-        var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
+      if (weather.cod === "404") {
+        // Display message to user
+        alert("City not found");
+        return;
+      }
 
-        fetch(onecallURL)
-          .then((data) => data.json())
-          .then(function (oneCallData) {
-            //   oneCallData has all the information that we need
-            console.log(oneCallData);
-          });
+      var lat = weather.coord.lat;
+      var lon = weather.coord.lon;
+      var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+      fetch(onecallURL)
+        .then((data) => data.json())
+        .then(function (oneCallData) {
+          //   oneCallData has all the information that we need
+          console.log(oneCallData);
+          var mainCard.classList.add("mainCard")
+
+          // Header
+          var cityEl = document.createElement("h2")
+          cityEl.textContent = city + new Date().toDateString();
+          mainCard.append(cityEl);
+
+          // UV Index
+          var uvEl = document.createElement("button");
+          var unIndex = oneCallData.current.uvi
+          uvEl.textContent = oneCallData.current.uvi
+          console.log(typeof oneCallData.current.uvi)
+          if (uvIndex<=2){
+            uvEl.classList.add("favourable");
+          }
+          else if (uvIndex>=5){
+            uvEl.classList.add("severe");
+          }
+          else {
+            uvEl.classList.add("moderate");
+        }
+          mainCard.append(uvEl);
+
+          $mastHead.innerHTML = "";
+          $mastHead.append(mainCard);
+
         });
-
+    });
 }
 
-getWeather("Mesa");
-getWeather("Dallas");
+
+function getFiveDay(city) {
+  var fiveDayFore = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=imperial`;
+
+  fetch(fiveDayFore)
+    .then((data) => data.json())
+    .then(function (oneCallData) {
+        //    has all the information that we need
+        console.log(oneCallData);
+          var fiveDayArray[index] = classList.add("mainCard")
+
+          var cityEl = document.createElement("h2")
+          cityEl.textContent = city + new Date().toDateString();
+          mainCard.append();
+        });
+// Degree symbol alt+ 0176
+
+
+// Date
+var dateEl = document.createElement("h2");
+dateEl.textContent = currentDay
+forecastCard.append(dateEl);
+
+// Icon
+var iconEl = document.createElement("img");
+iconEl.setAttribute("src", `http://openweather.org/img/wn/${currentDay.weather[0].icon}@2x.png`)
+forecastCard.append(iconEl);
+
+// Temperature
+var tempEl = document.createElement("p");
+tempEl.textContent = `Temp: ${currentDay.main.temp} °F`
+forecastCard.append(templEl);
+
+// Humidity
+var humidityEl = document.createElement("p");
+templEl.textContent = `Temp: ${currentDay.main.humidity} %`
+forecastCard.append(humidityEl);
+
+
+historyContainer.append(forecastCard)
+}
+
+
+historyContainer.addEventListener("click", function(e){
+  e.preventDefault();
+  if (!e.target.matches("li")) return;
+  getWeather(e.target.value);
+  getFiveDay(e.target.value);
+})
+
+$(".btn").on("click", function (event) {
+    //prevent refreshy
+    event.preventDefault();
+    console.log("submit");
+    var city = $("#city-input").val();
+    dhistory.pushState(city);
+    cityHandle(city);
+    console.log(city);
+    getWeather(city);
+    getFiveDay(city);
+
+  });
+
+  function cityHandle(cityname) {
+    // Treat as a catch all for each item in history array
+    if (!dhistory.includes(cityname)) {
+      dhistory.push(cityname)
+    };
+    localStorage.setItem("dhistory", JSON.stringify(past));
+    // Each item gets a p tag
+    // add event listener
+    // call getWeather() and buildDashboard using the value of
+    // event listener getFiveDay()
+    // push into history the most recent search
+    // take and store under history
+    
+  }
+
+  renderIt();
+  function renderIt(){
+    historyContainer.innerHTML = "";
+    for (const city of dhistory) {
+      var previousCity = document.createElement("li");
+      previousCity.textContent = city;
+      historyContainer.append(previousCity);
+    }
+  }
+  
+
+
+
+// var container = document.createElement("div");
+// var dayof5 = document.createElement("div");
+// var blok = document.createElement("p");
+// dayof5.textContent = blk.append(date, img, temp, humidity);
+
+// container.append(dayof5);
+
+// return container;
--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # Weather Dashboard
 
+## Links
+https://github.com/emacartoon/Weather-Dashboard
+https://emacartoon.github.io/Weather-Dashboard/
+
+
 ## API Links
 https://openweathermap.org/current
 
 https://openweathermap.org/forecast16
 
 https://openweathermap.org/api/one-call-api
+
+## User Story
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+- [x] THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+- [x] THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+- [x] THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+- [x] THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+- [x] THEN I am again presented with current and future conditions for that city
